71749265e732b94ec1ca8ef2e5d5e3dbd11e84f3
denistchob-ctrl <denis.tchob@gmail.com>
2026-08-31 17:19:16 -0300

    Update counting type selection and line position

--- Contando Pessoas v1.2.py
+++ Contando Pessoas v1.2.py
@@ -65,11 +65,17 @@ def desenhar_trajetoria(frame, trajectory, cor=(255, 0, 0), ultimos_pontos=30):
 
 model = YOLO("yolov8n.pt")
 
-vPadrao = "VERTICAL"  # "HORIZONTAL" ou "VERTICAL"
-#vPadrao = "HORIZONTAL"  # "HORIZONTAL" ou "VERTICAL"
+while tipo not in ["H", "V"]:
+    tipo = input("Escolha o tipo de contagem ([H]ORIZONTAL ou [V]ERTICAL): ").strip().upper()
+
+if tipo == "V":
+    vPadrao = "VERTICAL"  
+else:
+    vPadrao = "HORIZONTAL"
+    
 if vPadrao == "HORIZONTAL":
     VIDEO_PATH = "people.mp4"
-    LINE_Y = 300
+    LINE_Y = 150
     LINE_X = 0
 else:
     VIDEO_PATH = "paulista.mp4"
